ce355c9300b5af139a1209d73d81fdd3477c4337
Shin Dahye <64780560+dahye1013@users.noreply.github.com>
2022-06-07 18:18:08 +0900

    feat: globalstyle, container 화면 범위 세팅 (#8)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
-function App() {
-	return <div className="App">Hello Time Divider</div>
+import styled from 'styled-components'
+import GlobalStyle from './styles/GlobalStyle'
+const App = () => {
+	return (
+		<>
+			<GlobalStyle />
+			<Container>
+				<div className="App">
+					<h2>Hello Time Divider</h2>
+				</div>
+			</Container>
+		</>
+	)
 }
 
 export default App
+
+const Container = styled.div`
+	width: 100%;
+	max-width: 420px;
+	height: 100vh;
+	margin: auto;
+	padding: 0;
+	overflow: hidden;
+	background-color: #f1f0f7;
+`
---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,8 @@
+import reset from 'styled-reset'
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+      ${reset}
+    `
+
+export default GlobalStyle
