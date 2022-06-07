7a0785af477d6cfe0ccbb5700b1edb22920e0710
ljw0096 <ljw0096@naver.com>
2022-06-07 16:25:20 +0900

    feat: Button component 구현

--- src/components/Hello.js
+++
@@ -1,9 +0,0 @@
-const Hello = ({ children = 'Time Divider' }) => {
-	const helloStyle = {
-		textAlign: 'center',
-	}
-
-	return <h1 style={helloStyle}>Hello {children}</h1>
-}
-
-export default Hello
