ce8171ccf31b3ddf490bf756214076b82f60125a
ljw0096 <ljw0096@naver.com>
2022-06-07 18:33:28 +0900

    feat: Input 컴포넌트 구현

---
+++ src/stories/Input.stories.jsx
@@ -0,0 +1,10 @@
+import Input from '../components/Input'
+
+export default {
+	title: 'Component/Input',
+	component: Input,
+}
+
+export const Default = args => {
+	return <Input {...args}></Input>
+}
