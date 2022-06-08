7b3061cc1e1beecdcbf6f025ca58810d52cfc261
ljw0096 <ljw0096@naver.com>
2022-06-08 21:39:39 +0900

    fix: NavBar 뒤로가기 버튼 이탈 수정

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,24 +6,28 @@ const NavBarContainer = styled.div`
 	display: flex;
 	align-items: center;
 	justify-content: center;
+	width: 100%;
+	position: relative;
 	padding: 1.25rem;
 	border-bottom: 0.05rem solid #999;
 `
 
 const GoBackIcon = styled.a`
 	font-size: 2rem;
-	position: fixed;
+	position: absolute;
 	left: 1.5rem;
-	top: 2.5rem;
+	top: 1.25rem;
 	cursor: pointer;
 `
 
-const NavBar = ({ children = '시간 분배' }) => {
+const NavBar = ({ children = '시간 분배', backIcon, href }) => {
 	return (
 		<NavBarContainer>
-			<GoBackIcon href="/">
-				<IoIosArrowBack />
-			</GoBackIcon>
+			{backIcon && (
+				<GoBackIcon href={href}>
+					<IoIosArrowBack />
+				</GoBackIcon>
+			)}
 			<Text size="2">{children}</Text>
 		</NavBarContainer>
 	)
