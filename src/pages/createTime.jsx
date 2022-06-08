dc7ee9ccb41cea0ada4b2637df1d1f7a00e63a0e
dahye1013 <alrean@naver.com>
2022-06-08 16:18:20 +0900

    feat: 첫화면(home), 오늘의시간 페이지 ui 밑작업

--- src/pages/createTime.jsx
+++ src/pages/createTime.jsx
@@ -1,13 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 import { Link } from 'react-router-dom'
 
-const createTime = () => {
+import styled from 'styled-components'
+import NavBar from '../components/NavBar'
+import Text from '../components/Text'
+import Button from '../components/Button'
+import Input from '../components/Input'
+
+const CreateTime = () => {
+	const [task, setTask] = useState('')
+
 	return (
 		<>
-			<h2>오늘의 시간</h2>
-			<Link to="/createTodo">다음으로</Link>
+			<NavBar>오늘의 시간</NavBar>
+
+			<SubTitleArea>
+				<Text style={{ fontSize: '1rem' }}>오늘 사용할 수 있는 시간은 얼마인가요?</Text>
+			</SubTitleArea>
+
+			<Input type="text" value={task} onChange={e => setTask(e.target.value)}></Input>
+
+			<ButtonArea>
+				<Link to="/createTodo">
+					<Button disabled={true}>입력해주세요</Button>
+				</Link>
+			</ButtonArea>
 		</>
 	)
 }
 
-export default createTime
+export default CreateTime
+
+const SubTitleArea = styled.div`
+	height: 30vh;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+`
+
+const ButtonArea = styled.div`
+	position: absolute;
+	margin: 2rem 2rem;
+	width: 100%;
+	bottom: 1rem;
+`
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,14 +1,51 @@
 import React from 'react'
+
 import { Link } from 'react-router-dom'
 
-const home = () => {
+import styled from 'styled-components'
+import Button from '../components/Button'
+import Text from '../components/Text'
+import { IoIosHourglass } from 'react-icons/io'
+
+const Home = () => {
 	return (
 		<>
-			<h2>Home</h2>
-			<p>첫 화면 - 프로젝트 명</p>
-			<Link to="/createTime">시작하기</Link>
+			<Title>
+				<Text style={{ fontSize: '2rem' }}>일 모래시계</Text>
+			</Title>
+
+			<HourGlass>
+				<IoIosHourglass></IoIosHourglass>
+			</HourGlass>
+
+			<Text style={{ fontSize: '2rem' }}>시간을 관리해보세요!</Text>
+
+			<ButtonArea>
+				<Link to="/createTime">
+					<Button>시작하기</Button>
+				</Link>
+			</ButtonArea>
 		</>
 	)
 }
 
-export default home
+export default Home
+
+const Title = styled.h2`
+	padding: 2rem;
+`
+
+const HourGlass = styled.span`
+	height: 50vh;
+	font-size: 30vh;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`
+
+const ButtonArea = styled.div`
+	position: absolute;
+	margin: 2rem 2rem;
+	width: 100%;
+	bottom: 1rem;
+`
