b84753fa506bbfadb7280cc4b14730fe9cd61c1d
ljw0096 <ljw0096@naver.com>
2022-06-09 18:26:04 +0900

    feat: 각 Task Box 생성 및 클릭시 Input form 렌더링 구현

--- src/pages/createTimeDivider.jsx
+++ src/pages/createTimeDivider.jsx
@@ -1,11 +1,144 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import Button from '../components/Button'
+import NavBar from '../components/NavBar'
+import Input from '../components/Input'
+import Text from '../components/Text'
+
+const dummyTasks = [
+	{
+		id: '1',
+		task: '일하기',
+	},
+	{
+		id: '2',
+		task: '놀기',
+	},
+	{
+		id: '3',
+		task: '뛰기',
+	},
+	{
+		id: '4',
+		task: '걷기',
+	},
+]
+
+const BUTTON_TEXT = Object.freeze({
+	VALID: '다음 단계',
+	INVALID: '시간을 입력해주세요',
+})
+
+const ButtonArea = styled.div`
+	position: absolute;
+	margin: 2rem 2rem;
+	width: 100%;
+	bottom: 1rem;
+`
+
+const BoxContainer = styled.div`
+	display: flex;
+	width: 100%;
+	flex-wrap: wrap;
+`
+
+const BoxWrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	border: 1px solid;
+	overflow: hidden;
+	width: 8.6rem;
+`
+
+const Form = styled.form`
+	display: flex;
+	gap: 1rem;
+`
+
+const TaskInputBox = ({ task, ...props }) => {
+	return (
+		<BoxWrapper {...props}>
+			<div>{task.task}</div>
+			<Text>
+				{task.hour} : {task.minute}
+			</Text>
+		</BoxWrapper>
+	)
+}
 
 export const CreateTimeDivider = () => {
+	const [tasks, setTasks] = useState(
+		dummyTasks.map(task => {
+			return { ...task, hour: '00', minute: '00' }
+		}),
+	)
+	const [selectedTask, setSelectedTask] = useState('')
+
 	return (
 		<>
-			<h2>시간을 분배해요</h2>
-			<Link to="/updateDivider">다음으로</Link>
+			<NavBar backIcon>시간을 분배해요</NavBar>
+
+			<BoxContainer>
+				{tasks.map(task => (
+					<TaskInputBox
+						key={task.id}
+						task={task}
+						onClick={() => {
+							setSelectedTask(task)
+						}}
+					/>
+				))}
+			</BoxContainer>
+			{selectedTask && (
+				<Form
+					id={selectedTask.id}
+					onSubmit={e => {
+						e.preventDefault()
+						setTasks(
+							tasks.map(task => {
+								if (task.id === selectedTask.id) {
+									task.hour = selectedTask.hour
+									task.minute = selectedTask.minute
+								}
+								return task
+							}),
+						)
+						setSelectedTask('')
+					}}
+				>
+					<Input
+						style={{ width: '4rem' }}
+						type="text"
+						value={`${selectedTask.hour}`}
+						onChange={e => {
+							setSelectedTask({ ...selectedTask, hour: e.target.value })
+						}}
+						autoFocus={true}
+						required
+					/>
+					<Input
+						type="text"
+						style={{ width: '4rem' }}
+						value={`${selectedTask.minute}`}
+						onChange={e => {
+							setSelectedTask({ ...selectedTask, minute: e.target.value })
+						}}
+						required
+					/>
+
+					<Button size="sm" rect>
+						설정
+					</Button>
+				</Form>
+			)}
+			<ButtonArea>
+				<Link to="/updateDivider">
+					<Button>{BUTTON_TEXT.VALID}</Button>
+				</Link>
+			</ButtonArea>
 		</>
 	)
 }
