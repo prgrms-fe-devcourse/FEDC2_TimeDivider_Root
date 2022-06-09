cc1b7a8469cbbcf855e93b645a13af78f5eb5a37
Jiwon Lee <ljw0096@naver.com>
2022-06-10 01:29:51 +0900

    feat: 시간 분배 페이지 구현 (#17)
    * feat: 각 Task Box 생성 및 클릭시 Input form 렌더링 구현
    
    * feat: useLocation으로 이전 페이지 상태 받도록 구현
    
    * refactor: createTimeDivider 내부의 TaskBox 컴포넌트 분리
    
    * Refactor: createTimeDivider 페이지에서 TaskTimeForm 컴포넌트 분리
    
    * feat: 다음 페이지에 state로 task 전달 추가
    
    * feat: createTimeDivider 페이지 구현
    
    * chore: package-lock.json 파일 develop과 동일한  파일로 변경

--- src/pages/createTimeDivider.jsx
+++ src/pages/createTimeDivider.jsx
@@ -1,11 +1,97 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useLocation } from 'react-router-dom'
+import styled from 'styled-components'
+import Button from '../components/Button'
+import NavBar from '../components/NavBar'
+import TaskTimeForm from '../components/TaskTimeForm'
+import TaskBox from '../components/TaskBox'
+import Text from '../components/Text'
+
+const BUTTON_TEXT = Object.freeze({
+	VALID: '다음 단계',
+	INVALID: '시간을 입력해주세요',
+})
+
+const ButtonArea = styled.div`
+	position: absolute;
+	margin: 2rem 2rem;
+	width: 100%;
+	bottom: 1rem;
+`
+
+const BoxContainer = styled.div`
+	display: flex;
+	width: 100%;
+	flex-wrap: wrap;
+`
+
+const convertToSeconds = time => {
+	return parseInt(time.hours) * 3600 + parseInt(time.minutes) * 60
+}
+
+export const convertToHourMinute = time => {
+	const hour = String(parseInt(time / 3600))
+	const minute = String(parseInt((time % 3600) / 60))
+
+	return { hour, minute }
+}
 
 export const CreateTimeDivider = () => {
+	const location = useLocation()
+	const initalTotal = convertToSeconds(location.state.spareTime)
+	const [totalTime, setTotalTime] = useState(convertToSeconds(location.state.spareTime))
+
+	const [tasks, setTasks] = useState(
+		location.state.tasks.map(task => {
+			return { ...task, time: 0, hour: '0', minute: '0' }
+		}),
+	)
+	const [selectedTask, setSelectedTask] = useState(null)
+
+	const handleSubmit = selectedTask => {
+		const usedTime = convertToSeconds({ hours: selectedTask.hour, minutes: selectedTask.minute })
+		if (totalTime - usedTime < 0) {
+			setSelectedTask(null)
+			return
+		}
+		setTasks(
+			tasks.map(task => {
+				if (task.id === selectedTask.id) {
+					task.hour = selectedTask.hour
+					task.minute = selectedTask.minute
+					task.time = usedTime
+					setTotalTime(initalTotal - tasks.reduce((acc, task) => acc + task.time, 0))
+				}
+				return task
+			}),
+		)
+
+		setSelectedTask(null)
+	}
+
 	return (
 		<>
-			<h2>시간을 분배해요</h2>
-			<Link to="/updateDivider">다음으로</Link>
+			<NavBar backIcon>시간을 분배해요</NavBar>
+			<Text>
+				{convertToHourMinute(totalTime).hour} : {convertToHourMinute(totalTime).minute}
+			</Text>
+			<BoxContainer>
+				{tasks.map(task => (
+					<TaskBox
+						key={task.id}
+						task={task}
+						onClick={() => {
+							setSelectedTask(task)
+						}}
+					/>
+				))}
+			</BoxContainer>
+			{selectedTask && <TaskTimeForm targetTask={selectedTask} onSubmit={handleSubmit} />}
+			<ButtonArea>
+				<Link to="/updateDivider" state={{ tasks }}>
+					<Button>{BUTTON_TEXT.VALID}</Button>
+				</Link>
+			</ButtonArea>
 		</>
 	)
 }
