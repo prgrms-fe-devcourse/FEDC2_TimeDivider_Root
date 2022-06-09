efeff6f12dd9276afb665a62ff45985bdf0b105d
codeisneverodd <codeisneverodd@gmail.com>
2022-06-10 00:08:33 +0900

    feat: 상태값 로컬 스토리지에 저장되도록 변경

--- src/atom.js
+++ src/atom.js
@@ -1,15 +1,29 @@
 import { atom } from 'recoil'
+const localStorageEffect =
+	key =>
+	({ setSelf, onSet }) => {
+		const savedValue = localStorage.getItem(key)
+		if (savedValue != null) {
+			setSelf(JSON.parse(savedValue))
+		}
 
+		onSet((newValue, _, isReset) => {
+			isReset ? localStorage.removeItem(key) : localStorage.setItem(key, JSON.stringify(newValue))
+		})
+	}
 export const timerState = atom({
-	key: 'time',
+	key: 'timer',
 	default: {}, //{id:{time, name}}, anotherId: {time,name}}
+	effects: [localStorageEffect('timer')],
 })
 
 export const currentTimerState = atom({
 	key: 'currentTimer',
 	default: { id: 'root', name: '' },
+	effects: [localStorageEffect('currentTimer')],
 })
 export const combineState = atom({
 	key: 'combine',
 	default: { id: '', newExpiryTimestamp: undefined },
+	effects: [localStorageEffect('combine')],
 })
