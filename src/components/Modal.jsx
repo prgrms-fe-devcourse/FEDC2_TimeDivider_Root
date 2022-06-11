6a886e064511629507aeee507af52aaf07bbe86e
Jiwon Lee <ljw0096@naver.com>
2022-06-12 01:21:55 +0900

    feat: 모달 컴포넌트 구현 (#27)
    * feat: Modal 컴포넌트 UI 구현
    
    * feat: useClickAway hook 구현
    
    * feat: Modal width, height rem 입력 받도록 구현

---
+++ src/components/Modal.jsx
@@ -0,0 +1,67 @@
+import { useEffect, useMemo } from 'react'
+import styled from 'styled-components'
+import ReactDOM from 'react-dom'
+import useClickAway from '../hooks/useClickAway'
+
+const BackgroundDim = styled.div`
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100vw;
+	height: 100vh;
+	background-color: rgba(0, 0, 0, 0.5);
+	z-index: 1000;
+`
+const ModalContainer = styled.div`
+	position: fixed;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+	padding: 0.5rem;
+	border-radius: 0.5rem;
+	background-color: white;
+	box-shadow: 0 0.25rem 0.75rem rgba(0, 0, 0, 0.2);
+	box-sizing: border-box;
+`
+
+const CloseButton = styled.div`
+	position: fixed;
+	top: 0;
+	right: 1rem;
+	cursor: pointer;
+`
+
+const Modal = ({ children, width = 18.75, height, visible = false, onClose, ...props }) => {
+	const ref = useClickAway(() => {
+		onClose && onClose()
+	})
+
+	const containerStyle = useMemo(
+		() => ({
+			width: `${width}rem`,
+			height: `${height}rem`,
+		}),
+		[width, height],
+	)
+
+	const el = useMemo(() => document.createElement('div'), [])
+
+	useEffect(() => {
+		document.body.appendChild(el)
+		return () => {
+			document.body.removeChild(el)
+		}
+	})
+
+	return ReactDOM.createPortal(
+		<BackgroundDim style={{ display: visible ? 'block' : 'none' }}>
+			<ModalContainer ref={ref} {...props} style={{ ...props.style, ...containerStyle }}>
+				<CloseButton onClick={onClose}>x</CloseButton>
+				{children}
+			</ModalContainer>
+		</BackgroundDim>,
+		el,
+	)
+}
+
+export default Modal
---
+++ src/stories/Modal.stories.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import Modal from '../components/Modal'
+
+export default {
+	title: 'Component/Modal',
+	component: Modal,
+	argTypes: {
+		width: { defaultValue: 18.75, control: { type: 'number' } },
+		height: { control: { type: 'number' } },
+	},
+}
+
+export const Default = args => {
+	const [visible, setVisible] = useState(false)
+
+	return (
+		<div>
+			<button onClick={() => setVisible(true)}>show modal</button>
+			<Modal
+				{...args}
+				visible={visible}
+				onClose={() => {
+					setVisible(false)
+				}}
+			>
+				Hi
+			</Modal>
+		</div>
+	)
+}
