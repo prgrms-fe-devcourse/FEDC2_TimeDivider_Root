e3f6d0197df5e4e70585919ff9995b551ff3c402
codeisneverodd <codeisneverodd@gmail.com>
2022-06-12 01:23:02 +0900

    feat: 추가 클릭시 페이지 구현 및 시간 재생 에러 픽싱 (#28)
    * feat: createTimeDivider 페이지에서 상태를 생성하도록 수정
    
    * feat: 추가하기 페이지 구현
    
    * refactor: 시간 입력 select box로 변경
    
    * fix: 시간 재생이 안되던 오류 해결,
    feat: currentState 삭제,
    refactor: Timer의 onClick 분리

--- src/pages/addToDoTimeDivider.jsx
+++ src/pages/addToDoTimeDivider.jsx
@@ -1,11 +1,65 @@
 import React from 'react'
+import { useRecoilState } from 'recoil'
+import { timerState } from '../atom'
+import NavBar from '../components/NavBar'
+import { Timer } from '../components/Timer'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+import TimeSelectForm, { HOUR_NUMBERS, MINUTE_NUMBERS } from '../components/TimeSelectForm'
+import Select from '../components/Select'
 
 export const AddToDoTimeDivider = () => {
+	const [timers, setTimers] = useRecoilState(timerState)
 	return (
 		<>
-			<h2>AddToDoTimeDivider</h2>
+			<NavBar backIcon>모래시계 편집하기</NavBar>
+			<Link to="/doneTodo">완료하기</Link>
+			<Link to="/addTodo">추가하기</Link>
+			<TimerArea>
+				{Object.entries(timers).map(([id, { time, name }], index) => {
+					return (
+						<Timer
+							onClick={() => {}}
+							key={id}
+							id={id}
+							name={name}
+							expiryTimestamp={new Date(new Date().getTime() + time * 1000)}
+						/>
+					)
+				})}
+			</TimerArea>
+			<Modal>
+				<form
+					onSubmit={e => {
+						e.preventDefault()
+						const [name, time, id] = [
+							e.target.name.value,
+							e.target.hour.value * 60 * 60 + e.target.minute.value * 60,
+							'' + Date.now(),
+						]
+						setTimers({ ...timers, [id]: { time, name } })
+					}}
+				>
+					할 일: <input name={'name'} required={true} type={'text'} maxLength={10} />
+					몇 시간 : <Select name={'hour'} data={HOUR_NUMBERS} />
+					몇 분: <Select name={'minute'} data={MINUTE_NUMBERS} />
+					<button>생성하기</button>
+				</form>
+			</Modal>
 		</>
 	)
 }
 
 export default AddToDoTimeDivider
+const TimerArea = styled.div`
+	display: flex;
+	justify-content: space-between;
+	flex-wrap: wrap;
+	width: 100%;
+	height: 30rem;
+`
+const Modal = styled.div`
+	width: 100%;
+	height: 10rem;
+	background-color: aliceblue;
+`
--- src/pages/updateTimeDivider.jsx
+++ src/pages/updateTimeDivider.jsx
@@ -1,32 +1,31 @@
 import React, { useEffect } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import styled from 'styled-components'
 import NavBar from '../components/NavBar'
 import { useRecoilState } from 'recoil'
-import { timerState } from '../atom'
+import { currentTimerIdState, currentTimerState, timerState } from '../atom'
 import { Timer } from '../components/Timer'
 const UpdateTimeDivider = () => {
-	const dummyData = [
-		{ id: '123', name: '밥 묵자', time: 600 },
-		{ id: '234', name: '치킨 묵자', time: 1200 },
-		{ id: '345', name: '피자 묵자', time: 1800 },
-		{ id: '456', name: '삼겹살 묵자', time: 2400 },
-		{ id: '567', name: '홍차 마시자', time: 3000 },
-		{ id: '678', name: '잠이나 자자', time: 3600 },
-	]
 	const [timers, setTimers] = useRecoilState(timerState)
-
-	useEffect(() => {
-		const newTimers = {}
-		dummyData.forEach(({ id, name, time }) => (newTimers[id] = { name, time }))
+	const stopAllTimers = () => {
+		const newTimers = Object.assign({}, timers)
+		for (const timerId in newTimers) {
+			newTimers[timerId] = {
+				...newTimers[timerId],
+				isRunning: false,
+			}
+		}
 		setTimers(newTimers)
-	}, [])
-
+	}
 	return (
 		<>
 			<NavBar backIcon>모래시계 편집하기</NavBar>
-			<Link to="/doneTodo">완료하기</Link>
-			<Link to="/addTodo">추가하기</Link>
+			<Link to="/doneTodo" onClick={() => stopAllTimers()}>
+				완료하기
+			</Link>
+			<Link to="/addTodo" onClick={() => stopAllTimers()}>
+				추가하기
+			</Link>
 			<TimerArea>
 				{Object.entries(timers).map(([id, { time, name }], index) => (
 					<Timer
@@ -34,6 +33,16 @@ const UpdateTimeDivider = () => {
 						id={id}
 						name={name}
 						expiryTimestamp={new Date(new Date().getTime() + time * 1000)}
+						onClick={() => {
+							const newTimers = Object.assign({}, timers)
+							for (const timerId in newTimers) {
+								newTimers[timerId] = {
+									...newTimers[timerId],
+									isRunning: timerId === id ? !newTimers[id].isRunning : false,
+								}
+							}
+							setTimers(newTimers)
+						}}
 					/>
 				))}
 			</TimerArea>
