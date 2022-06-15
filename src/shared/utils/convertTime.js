d1324b1770dfa417a6874ba0798f9431aee34d9d
Jiwon Lee <ljw0096@naver.com>
2022-06-15 18:10:17 +0900

    refactor: addZero 시간 포맷팅 함수 utils 분리 (#58)

--- src/shared/utils/convertTime.js
+++ src/shared/utils/convertTime.js
@@ -1,10 +1,12 @@
+import { addZero } from './timeFormatting'
+
 export const convertHourMinuteToSeconds = time => {
 	return parseInt(time.hour) * 3600 + parseInt(time.minute) * 60
 }
 
 export const convertSecondsToHourMinute = time => {
-	const hour = String(parseInt(time / 3600))
-	const minute = String(parseInt((time % 3600) / 60))
+	const hour = addZero(String(parseInt(time / 3600)))
+	const minute = addZero(String(parseInt((time % 3600) / 60)))
 
 	return { hour, minute }
 }
---
+++ src/shared/utils/timeFormatting.js
@@ -0,0 +1,3 @@
+export const addZero = time => {
+	return time.length === 1 ? `0${time}` : time
+}
