5ea6de21578528b07415595d8e590247cc439cbc
dahye1013 <alrean@naver.com>
2022-06-17 00:31:17 +0900

    chore: 색상 대문자로 수정

--- src/shared/constants/colors.js
+++ src/shared/constants/colors.js
@@ -4,7 +4,7 @@ export const colors = {
 	completeGreen: '#5ABF83',
 	timeoutDarkGray: '#505866',
 	lightGray: '#919191',
-	brightGray: '#f5f5f5',
+	brightGray: '#F5F5F5',
 	black: '#000000',
 	white: '#FFFFFF',
 }
