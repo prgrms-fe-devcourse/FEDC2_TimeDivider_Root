537a0e8f739be02bf30a4a6b1f66a8c0006818c9
dahye1013 <alrean@naver.com>
2022-06-17 21:49:43 +0900

    feat: login, signup 비동기 처리

--- src/shared/api/apis/authApis.js
+++ src/shared/api/apis/authApis.js
@@ -1,30 +1,26 @@
 import API from '../API'
 
-export const login = (userInfo = { email: null, password: null }) => {
-	API.post('/login', userInfo)
-		.then(({ data }) => {
-			const { user, token } = data
-			alert('로그인에 성공했습니다.')
-			return { user, token }
-		})
-		.catch(err => {
-			alert('로그인에 실패했습니다.')
-			console.error(err)
-		})
+export const login = async (userInfo = { email: null, password: null }) => {
+	try {
+		const { data } = await API.post('/login', userInfo)
+		const { user, token } = data
+		alert('로그인에 성공했습니다.')
+		return { user, token }
+	} catch (err) {
+		alert('로그인에 실패했습니다.')
+	}
 }
 
-export const signup = (userInfo = { email: null, fullName: 'unknown', password: null }) => {
+export const signup = async (userInfo = { email: null, fullName: 'unknown', password: null }) => {
 	// INFO: 회원가입시 이름 외 Email, Password 입력하도록 임시처리
 	userInfo.fullName = userInfo.email
 
-	API.post('/signup', userInfo)
-		.then(res => {
-			const { user, token } = res.data
-			alert('회원가입에 성공했습니다.')
-			return { user, token }
-		})
-		.catch(err => {
-			alert('회원가입에 실패했습니다.')
-			console.error(err)
-		})
+	try {
+		const { data } = await API.post('/signup', userInfo)
+		const { user, token } = data
+		alert('회원가입에 성공했습니다.')
+		return { user, token }
+	} catch (err) {
+		alert('회원가입에 실패했습니다.')
+	}
 }
