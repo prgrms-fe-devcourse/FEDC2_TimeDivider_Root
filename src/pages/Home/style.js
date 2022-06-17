d825657a94e6232415b9b7e0255093b0ea68cc1e
Shin Dahye <64780560+dahye1013@users.noreply.github.com>
2022-06-17 09:25:12 +0900

    refactor: page 디렉토리 관리 (#64)

---
+++ src/pages/Home/style.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	width: 100%;
+	height: 100%;
+	align-items: center;
+	justify-content: flex-end;
+	padding: 3.3rem;
+`
+
+export const Logo = styled.img`
+	position: absolute;
+	left: 7rem;
+	top: 17.4rem;
+	height: 23.2rem;
+	width: 22.8rem;
+`
+
+export const SubTitle = styled.span`
+	margin: 0 8rem;
+	line-height: 4.6rem;
+	text-align: center;
+	height: 23.2rem;
+`
+
+export const ButtonArea = styled.div`
+	display: flex;
+	justify-content: center;
+	position: absolute;
+	margin: 2rem 2rem;
+	width: 100%;
+	bottom: 1rem;
+`
