ac6f2786d89b5de15978a5e2dd5d27110faa5def
dahye1013 <alrean@naver.com>
2022-06-17 22:44:43 +0900

    feat: 로그인 성공시 home 화면으로 이동

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -2,12 +2,15 @@ import LoginForm from './components/LoginForm'
 import apis from 'shared/api'
 import { useSetRecoilState } from 'recoil'
 import { loginUserState } from 'state/user'
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
+	const navigate = useNavigate()
 	const setUserInfo = useSetRecoilState(loginUserState)
 	const handleLogin = async loginInfo => {
 		const result = await apis.login(loginInfo)
 		setUserInfo(result)
+		navigate('/home')
 	}
 	return (
 		<>
