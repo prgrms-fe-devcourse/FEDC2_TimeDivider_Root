74b17c2dd845a9cccaad301530a550c067d06887
codeisneverodd <codeisneverodd@gmail.com>
2022-06-18 02:23:32 +0900

    etc: 테스트용 코드 삭제

--- src/pages/UpdateTimeDivider/index.js
+++ src/pages/UpdateTimeDivider/index.js
@@ -1,11 +1,11 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { useRecoilState, useSetRecoilState } from 'recoil'
 import { addMode, defaultMode, doneMode, mergeMode, modeState, originIdState } from 'state/timer'
 import NavBar from 'shared/components/NavBar'
 import Timer from 'shared/components/Timer'
 import AddFormModal from 'pages/UpdateTimeDivider/components/AddFormModal'
 import MergeFormModal from 'pages/UpdateTimeDivider/components/MergeFormModal'
-import DoneFormModal from 'pages/UpdateTimeDivider/components/DoneFormModal'
+import CompleteFormModal from 'pages/UpdateTimeDivider/components/CompleteFormModal'
 import { BottomBar } from 'shared/components/BottomBar'
 import { BottomBarArea, Description, TimerArea, ToolBar, Wrapper } from './style'
 import { useTimers } from 'shared/hooks/useTimers'
@@ -13,22 +13,18 @@ import { ToolBarButton } from './components/ToolBarButton'
 
 const UpdateTimeDivider = () => {
 	const { timers, toggleRunning } = useTimers()
-	const [completeMode, setCompleteMode] = useState(false)
 	const [mode, setMode] = useRecoilState(modeState)
 	const setOriginId = useSetRecoilState(originIdState)
 
 	const handleTimerClick = id => {
 		if (timers[id].disabled) return
-		completeMode ? setOriginId(id) : toggleRunning(id)
+		mode === doneMode ? setOriginId(id) : toggleRunning(id)
 	}
 	const handleAddButtonClick = e => {
 		toggleRunning()
 		setMode(addMode)
-		setCompleteMode(false)
 	}
 	const handleCompleteButtonClick = e => {
-		toggleRunning()
-		setCompleteMode(x => !x)
 		mode === doneMode ? setMode(defaultMode) : setMode(doneMode)
 	}
 	const timerEntries = timers =>
@@ -43,12 +39,12 @@ const UpdateTimeDivider = () => {
 			<NavBar>제목 미정 </NavBar>
 			<ToolBar>
 				<ToolBarButton onClick={handleAddButtonClick}>{'추가'}</ToolBarButton>
-				<ToolBarButton reversed={completeMode} onClick={handleCompleteButtonClick}>
-					{completeMode ? '취소' : '완료'}
+				<ToolBarButton reversed={mode === doneMode} onClick={handleCompleteButtonClick}>
+					{mode === doneMode ? '취소' : '완료'}
 				</ToolBarButton>
 			</ToolBar>
 			<Description>
-				{completeMode ? '완료 할 일을 선택하세요.' : '일을 클릭하여 시작하세요.'}
+				{mode === doneMode ? '완료 할 일을 선택하세요.' : '일을 클릭하여 시작하세요.'}
 			</Description>
 			<TimerArea>
 				{timerEntries(timers).map(([id, { time, name }], index) => (
@@ -62,7 +58,7 @@ const UpdateTimeDivider = () => {
 				))}
 			</TimerArea>
 			{mode === addMode && <AddFormModal />}
-			{mode === doneMode && <DoneFormModal />}
+			{mode === doneMode && <CompleteFormModal />}
 			{mode === mergeMode && <MergeFormModal />}
 			<BottomBarArea>
 				<BottomBar />
