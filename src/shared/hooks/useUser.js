9a4e9da0ee89186237681e0dcda3fb343357589d
dahye1013 <alrean@naver.com>
2022-06-21 22:03:42 +0900

    fix: merge 오류 수정

--- src/shared/hooks/useUser.js
+++ src/shared/hooks/useUser.js
@@ -1,63 +1,66 @@
-import styled from 'styled-components'
-import { Link, useNavigate } from 'react-router-dom'
+import { useRecoilState, useResetRecoilState } from 'recoil'
+import { loginDataState, userState } from '../../state/user'
+import { requestLogout } from '../api/apis/authApis'
+import { useTimers } from 'shared/hooks/useTimers'
+import { login as requestLogin } from 'shared/api/apis/authApis'
+import { getUser, requestChangeFullName, uploadImage } from '../api/apis/userApis'
 
-import logo from 'shared/images/logoTimeDivider.png'
-import LoginForm from './components/LoginForm'
-import Divider from './components/Divider'
-import Text from 'shared/components/Text'
-import NavBar from 'shared/components/NavBar'
-import Button from 'shared/components/Button'
-import { colors } from 'shared/constants/colors'
+const dummyUserImage = 'https://tva1.sinaimg.cn/large/e6c9d24egy1h3g25xp63rj20e80e8gm1.jpg'
+export const useUser = () => {
+	const { resetTimers } = useTimers()
+	const [loginData, setLoginData] = useRecoilState(loginDataState)
+	const [user, setUser] = useRecoilState(userState)
+	const removeLoginData = useResetRecoilState(loginDataState)
 
-import apis from 'shared/api'
-import { useUser } from 'shared/hooks/useUser'
+	const isLoggedIn = loginData.token !== null
 
-const Login = () => {
-	const { setLoginData } = useUser()
-	const navigate = useNavigate()
-	const handleLogInSubmit = async loginInfo => {
-		const { isSuccess, message, ...loginedInfo } = await apis.login(loginInfo)
-
-		if (isSuccess) {
-			setLoginData(loginedInfo)
-			alert(message)
-			navigate('/updateTimeDivider')
-		}
-
-		if (!isSuccess) {
-			alert(message)
-		}
+	const changeName = async fullName => {
+		await requestChangeFullName(fullName)
 	}
-	return (
-		<>
-			<NavBar backIcon />
-			<Logo src={logo}> </Logo>{' '}
-			<Wrapper>
-				<LoginForm onSubmit={handleLogInSubmit} />
-				<Divider type={'horizontal'}> </Divider>
-				<ButtonArea>
-					<Text block={true} size={1.3} textAlign={'start'} color={colors.lightGray}>
-						회원이 아니신가요 ?
-					</Text>{' '}
-					<Link to="/signup">
-						<Button backgroundColor={'#fff'} fontColor={colors.blue}>
-							회원가입{' '}
-						</Button>{' '}
-					</Link>{' '}
-					<Link to="/home">
-						<Button backgroundColor={'#fff'} borderColor={'#505866'} fontColor={'#505866'}>
-							비회원으로 이용하기{' '}
-						</Button>{' '}
-					</Link>{' '}
-				</ButtonArea>{' '}
-			</Wrapper>{' '}
-		</>
-	)
+
+	const logout = async () => {
+		const response = await requestLogout()
+		removeLoginData()
+		resetTimers()
+		return response
+	}
+	const changeImage = async image => {
+		const formData = new FormData()
+		formData.append('image', image)
+		formData.append('isCover', false)
+		await uploadImage(formData)
+	}
+
+	const refreshUser = async () => {
+		const response = await getUser(user._id)
+		setUser({
+			...response.data,
+			image: response.data.image ?? dummyUserImage,
+		})
+	}
+	const login = async (userInfo = { email: null, password: null }) => {
+		const { isSuccess, message, ...newLoginData } = await requestLogin(userInfo)
+		if (isSuccess) {
+			setLoginData(newLoginData)
+			setUser({ ...newLoginData.user, image: newLoginData.user.image ?? dummyUserImage })
+		}
+		return { isSuccess, message }
+	}
+
+	return {
+		user,
+		isLoggedIn,
+		login,
+		changeName,
+		changeImage,
+		refreshUser,
+		logout,
+	}
 }
 
 export default Login
 
-export const Logo = styled.img`
+export const Logo = styled.img `
 	left: 7rem;
 	top: 17.4rem;
 	height: 12rem;
@@ -65,7 +68,7 @@ export const Logo = styled.img`
 	margin: 5rem;
 `
 
-const Wrapper = styled.div`
+const Wrapper = styled.div `
 	height: 100%;
 	display: flex;
 	flex-direction: column;
@@ -73,7 +76,7 @@ const Wrapper = styled.div`
 	align-items: center;
 `
 
-const ButtonArea = styled.div`
+const ButtonArea = styled.div `
 	height: 100%;
 	display: flex;
 	flex-direction: column;
