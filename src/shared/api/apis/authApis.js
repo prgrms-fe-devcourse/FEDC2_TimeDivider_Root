f69fdf9c610d2556efa10bfada82accc59dcfa49
dahye1013 <alrean@naver.com>
2022-06-22 00:03:28 +0900

    refactor: api 요청 메소드명 수정

--- src/shared/api/apis/authApis.js
+++ src/shared/api/apis/authApis.js
@@ -1,29 +1,29 @@
 import API from '../API'
 
-export const requestLogin = async(userInfo = { email: null, password: null }) => {
-    const { isSuccess, message, data = {} } = await API.post('/login', userInfo)
-    if (!isSuccess) {
-        return { isSuccess, message }
-    }
-    const { user, token } = data
-    return { isSuccess, user, token, message: '로그인에 성공했습니다.' }
+export const requestLogin = async (userInfo = { email: null, password: null }) => {
+	const { isSuccess, message, data = {} } = await API.post('/login', userInfo)
+	if (!isSuccess) {
+		return { isSuccess, message }
+	}
+	const { user, token } = data
+	return { isSuccess, user, token, message: '로그인에 성공했습니다.' }
 }
 
-export const requestSignup = async(
-    userInfo = { email: null, fullName: 'unknown', password: null },
+export const requestSignup = async (
+	userInfo = { email: null, fullName: 'unknown', password: null },
 ) => {
-    const { isSuccess, message, data = {} } = await API.post('/signup', userInfo)
+	const { isSuccess, message, data = {} } = await API.post('/signup', userInfo)
 
-    if (!isSuccess) {
-        return { isSuccess, message }
-    }
+	if (!isSuccess) {
+		return { isSuccess, message }
+	}
 
-    const { user, token } = data
-    return { isSuccess, user, token, message: '회원가입에 성공했습니다.' }
+	const { user, token } = data
+	return { isSuccess, user, token, message: '회원가입에 성공했습니다.' }
 }
 
-export const requestLogout = async() => {
-    const { isSuccess, data } = await API.post('/logout')
+export const requestLogout = async () => {
+	const { isSuccess, data } = await API.post('/logout')
 
-    return { isSuccess, message: data }
+	return { isSuccess, message: data }
 }
