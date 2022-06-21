44be70618d3f794356e8c06beead2674dad49d7e
Shin Dahye <64780560+dahye1013@users.noreply.github.com>
2022-06-21 15:44:01 +0900

    fix: 오늘의시간, 오늘의 할일 페이지 전역 상태관리로 수정 (#93)
    * feat: subtitle 컴포넌트 추가
    
    * feat: TimeSelectionForm 컴포넌트 추가
    
    * refactor: 오늘의시간 페이지 컴포넌트 기반 리팩토링
    
    * refactor: 오늘의시간 페이지 커스텀 훅 기반 리팩토링
    
    * fix: 라우팅 링크 수정
    
    * feat: timer 생성용 atom 추가
    
    * refactor: 할일만들기 페이지 커스텀훅 추가
    
    * refactor: 할일만들기 페이지 커스텀훅 기반 리팩토링
    
    * chore: 불필요한 인자 및 제거 및 함수명 수정
    
    * remove: 미사용 할일만들기 페이지 제거
    
    * refactor: 초기화정보 라우팅에서 전역상태 호출로 이관

---
+++ src/pages/CreateSpareTime/index.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import * as S from './style'
+import NavBar from 'shared/components/NavBar'
+import Button from 'shared/components/Button'
+import SubTitle from 'shared/components/SubTitle'
+import TimeSelectForm from './components/TimeSelecForm'
+import useCreatingTimers from 'shared/hooks/useCreatingTimers'
+
+const BUTTON_TEXT = Object.freeze({
+	VALID: '다음 단계',
+	INVALID: '시간을 입력해주세요',
+})
+
+const CreateSpareTime = () => {
+	const { spareTime, updateSpareTime, isValidSpareTime } = useCreatingTimers()
+
+	return (
+		<S.Wrapper>
+			<NavBar backIcon />
+			<SubTitle>오늘 사용할 수 있는 시간은 얼마인가요?</SubTitle>
+			<TimeSelectForm intialTime={spareTime} handleChangeTime={updateSpareTime}></TimeSelectForm>
+			<S.ButtonArea>
+				<Link to="/createTimerName" state={{ spareTime }}>
+					<Button disabled={!isValidSpareTime}>
+						{!isValidSpareTime ? BUTTON_TEXT.INVALID : BUTTON_TEXT.VALID}
+					</Button>
+				</Link>
+			</S.ButtonArea>
+		</S.Wrapper>
+	)
+}
+
+export default CreateSpareTime
