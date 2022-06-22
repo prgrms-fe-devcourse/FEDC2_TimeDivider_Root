1c6e1805123ac389e00ed66242bb3b79fd8a00b8
codeisneverodd <codeisneverodd@gmail.com>
2022-06-22 19:37:00 +0900

    feat: 로그인 정보가 없을 때, 로그인으로 이동 버튼을 표시

--- src/pages/ShareTask/index.js
+++ src/pages/ShareTask/index.js
@@ -7,11 +7,18 @@ import usePosts from 'shared/hooks/usePosts'
 import AvatarItem from './components/AvatarItem'
 import { AvatarListArea, ButtonArea, CardArea, Footer, Wrapper } from './style'
 import { colors } from '../../shared/constants/colors'
+import { Description, NotLoggedInWrapper } from '../MyPage/style'
+import Text from '../../shared/components/Text'
+import { useUser } from '../../shared/hooks/useUser'
 
 const ShareTask = () => {
 	const navigate = useNavigate()
-
+	const { isLoggedIn } = useUser()
 	const { posts, isLoading, updateMyPost, makePrivateMyPost } = usePosts()
+
+	const handleLogin = () => {
+		navigate('/')
+	}
 
 	const handleUpdateButtonClick = async () => {
 		updateMyPost()
@@ -27,64 +34,78 @@ const ShareTask = () => {
 
 	return (
 		<Wrapper>
-			<ButtonArea>
-				<Button
-					width={8}
-					height={3}
-					fontSize={1.3}
-					backgroundColor={colors.white}
-					fontColor={colors.timeoutDarkGray}
-					borderColor={colors.timeoutDarkGray}
-					hoverColor={colors.red}
-					style={{ boxShadow: 'none' }}
-					onClick={handleNotShareButtonClick}
-				>
-					공유중지
-				</Button>
-				<Button
-					width={8}
-					height={3}
-					fontSize={1.3}
-					style={{ boxShadow: 'none' }}
-					onClick={handleUpdateButtonClick}
-				>
-					업데이트
-				</Button>
-			</ButtonArea>
-			<AvatarListArea>
-				{posts.map(post => (
-					<AvatarItem
-						onClick={() => {
-							handleNavigate(post._id)
-						}}
-						key={post._id}
-						postId={post._id}
-						imageSrc={post.imageSrc}
-						username={post.author.fullName}
-					/>
-				))}
-			</AvatarListArea>
+			{!isLoggedIn && (
+				<NotLoggedInWrapper>
+					<Description>
+						<Text style={{ wordBreak: 'keep-all', fontSize: '2.2rem', lineHeight: '1.5' }}>
+							게시판을 이용하려면 로그인하여야합니다.
+						</Text>
+					</Description>
+					<Button onClick={handleLogin}>로그인 페이지로 이동</Button>
+				</NotLoggedInWrapper>
+			)}
+			{isLoggedIn && (
+				<>
+					<ButtonArea>
+						<Button
+							width={8}
+							height={3}
+							fontSize={1.3}
+							backgroundColor={colors.white}
+							fontColor={colors.timeoutDarkGray}
+							borderColor={colors.timeoutDarkGray}
+							hoverColor={colors.red}
+							style={{ boxShadow: 'none' }}
+							onClick={handleNotShareButtonClick}
+						>
+							공유중지
+						</Button>
+						<Button
+							width={8}
+							height={3}
+							fontSize={1.3}
+							style={{ boxShadow: 'none' }}
+							onClick={handleUpdateButtonClick}
+						>
+							업데이트
+						</Button>
+					</ButtonArea>
+					<AvatarListArea>
+						{posts.map(post => (
+							<AvatarItem
+								onClick={() => {
+									handleNavigate(post._id)
+								}}
+								key={post._id}
+								postId={post._id}
+								imageSrc={post.imageSrc}
+								username={post.author.fullName}
+							/>
+						))}
+					</AvatarListArea>
 
-			<CardArea>
-				{isLoading ? (
-					<div>로딩중</div>
-				) : (
-					posts.map(post => (
-						<PostCard
-							key={post._id}
-							id={post._id}
-							author={post.author.fullName}
-							timers={post.timers || []}
-							like={post.like}
-							likeId={post.likeId}
-							size={'md'}
-							onComentClick={handleNavigate}
-							imageSrc={post.imageSrc}
-							comments={post.comments}
-						/>
-					))
-				)}
-			</CardArea>
+					<CardArea>
+						{isLoading ? (
+							<div>로딩중</div>
+						) : (
+							posts.map(post => (
+								<PostCard
+									key={post._id}
+									id={post._id}
+									author={post.author.fullName}
+									timers={post.timers || []}
+									like={post.like}
+									likeId={post.likeId}
+									size={'md'}
+									onComentClick={handleNavigate}
+									imageSrc={post.imageSrc}
+									comments={post.comments}
+								/>
+							))
+						)}
+					</CardArea>
+				</>
+			)}
 			<Footer>
 				<BottomBar />
 			</Footer>
