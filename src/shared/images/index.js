4c48912803eaf7027b94a2cfa8c6116c954bb26f
dahye1013 <alrean@naver.com>
2022-06-30 20:26:27 +0900

    refactor: shared 컴포넌트, 이미지 module export, import 수정

---
+++ src/shared/images/index.js
@@ -0,0 +1,6 @@
+export { default as board } from './board.png'
+export { default as clock } from './clock.png'
+export { default as dummyUser } from './dummyUser.png'
+export { default as loading } from './loading.gif'
+export { default as logoTimeDivider } from './logoTimeDivider.png'
+export { default as profile } from './profile.png'
