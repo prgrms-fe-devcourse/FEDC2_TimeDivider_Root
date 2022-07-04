24778c918f9f544c2ac01852a1f3c3a81204c5c1
codeisneverodd <codeisneverodd@gmail.com>
2022-07-04 22:47:31 +0900

    feat: MyPage index style 통합

--- src/pages/MyPage/style.js
+++ src/pages/MyPage/style.js
@@ -1,95 +1,95 @@
-import styled from 'styled-components'
-import { colors, themeColors } from '../../shared/constants/colors'
-import Text from '../../shared/components/Text'
-
-export const Wrapper = styled.div`
-	position: relative;
-	width: 100%;
-	height: 100%;
-	background-color: ${themeColors.labelBackground};
-`
-export const Profiles = styled.div`
-	display: flex;
-	width: 100%;
-	margin-bottom: 2.7rem;
-	flex-direction: column;
-	align-items: center;
-	row-gap: 1rem;
-`
-export const Avatar = styled.div`
-	width: ${props => props.size}rem;
-	height: ${props => props.size}rem;
-	background: no-repeat top center url('${props => props.src}');
-	background-size: cover;
-	border-radius: 50%;
-`
-export const Settings = styled.div`
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	justify-content: center;
-	row-gap: 1rem;
-	width: 100%;
-`
-export const Setting = styled.div`
-	box-sizing: border-box;
-	display: flex;
-	column-gap: 1rem;
-	width: 33.3rem;
-	height: 5rem;
-	border-radius: 1.1rem;
-	align-items: center;
-	padding: 1.5rem;
-	background-color: ${colors.white};
-`
-export const Icon = styled(Text)`
-	display: block;
-	width: 2rem;
-	height: 2rem;
-`
-export const Title = styled(Text)`
-	display: block;
-	flex: 1;
-`
-export const BottomBarArea = styled.div`
-	width: 100%;
-	position: absolute;
-	bottom: 0;
-	left: 0;
-`
-export const LogOutWrapper = styled.div`
-	box-sizing: border-box;
-	display: flex;
-	width: 100%;
-	justify-content: end;
-	margin-top: 2.4rem;
-	margin-bottom: 3.6rem;
-	padding-right: 2.1rem;
-`
-export const LogOut = styled.div`
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	width: 8.2rem;
-	height: 2.6rem;
-	border-radius: 0.6rem;
-	font-size: 1.3rem;
-	background-color: ${colors.white};
-	color: ${colors.red};
-	cursor: pointer;
-`
-
-export const Description = styled.div`
-	width: 24.2rem;
-	height: 9rem;
-	text-align: center;
-	margin-top: 15.6rem;
-	margin-bottom: 13.6rem;
-`
-export const NotLoggedInWrapper = styled.div`
-	display: flex;
-	flex-direction: column;
-	width: 100%;
-	height: 100%;
-	align-items: center;
-`
+// import styled from 'styled-components'
+// import { colors, themeColors } from '../../shared/constants/colors'
+// import Text from '../../shared/components/Text'
+//
+// export const Wrapper = styled.div`
+// 	position: relative;
+// 	width: 100%;
+// 	height: 100%;
+// 	background-color: ${themeColors.labelBackground};
+// `
+// export const Profiles = styled.div`
+// 	display: flex;
+// 	width: 100%;
+// 	margin-bottom: 2.7rem;
+// 	flex-direction: column;
+// 	align-items: center;
+// 	row-gap: 1rem;
+// `
+// export const Avatar = styled.div`
+// 	width: ${props => props.size}rem;
+// 	height: ${props => props.size}rem;
+// 	background: no-repeat top center url('${props => props.src}');
+// 	background-size: cover;
+// 	border-radius: 50%;
+// `
+// export const Settings = styled.div`
+// 	display: flex;
+// 	flex-direction: column;
+// 	align-items: center;
+// 	justify-content: center;
+// 	row-gap: 1rem;
+// 	width: 100%;
+// `
+// export const Setting = styled.div`
+// 	box-sizing: border-box;
+// 	display: flex;
+// 	column-gap: 1rem;
+// 	width: 33.3rem;
+// 	height: 5rem;
+// 	border-radius: 1.1rem;
+// 	align-items: center;
+// 	padding: 1.5rem;
+// 	background-color: ${colors.white};
+// `
+// export const Icon = styled(Text)`
+// 	display: block;
+// 	width: 2rem;
+// 	height: 2rem;
+// `
+// export const Title = styled(Text)`
+// 	display: block;
+// 	flex: 1;
+// `
+// export const BottomBarArea = styled.div`
+// 	width: 100%;
+// 	position: absolute;
+// 	bottom: 0;
+// 	left: 0;
+// `
+// export const LogOutWrapper = styled.div`
+// 	box-sizing: border-box;
+// 	display: flex;
+// 	width: 100%;
+// 	justify-content: end;
+// 	margin-top: 2.4rem;
+// 	margin-bottom: 3.6rem;
+// 	padding-right: 2.1rem;
+// `
+// export const LogOut = styled.div`
+// 	display: flex;
+// 	justify-content: center;
+// 	align-items: center;
+// 	width: 8.2rem;
+// 	height: 2.6rem;
+// 	border-radius: 0.6rem;
+// 	font-size: 1.3rem;
+// 	background-color: ${colors.white};
+// 	color: ${colors.red};
+// 	cursor: pointer;
+// `
+//
+// export const Description = styled.div`
+// 	width: 24.2rem;
+// 	height: 9rem;
+// 	text-align: center;
+// 	margin-top: 15.6rem;
+// 	margin-bottom: 13.6rem;
+// `
+// export const NotLoggedInWrapper = styled.div`
+// 	display: flex;
+// 	flex-direction: column;
+// 	width: 100%;
+// 	height: 100%;
+// 	align-items: center;
+// `
