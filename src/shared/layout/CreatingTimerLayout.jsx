f69d693fd4eb9c0ca9bf08ed1b9a3f465f56b8b1
dahye1013 <alrean@naver.com>
2022-07-05 12:34:22 +0900

    feat: timer 생성 프로세스 페이지 레이아웃 컴포넌트 추가

---
+++ src/shared/layout/CreatingTimerLayout.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import styled from 'styled-components'
+import { NavBar, Button, SubTitle } from 'shared/components'
+import { Link } from 'react-router-dom'
+
+const CreatingTimerLayout = ({
+	children,
+	subTitleText,
+	description,
+	nextStepLink,
+	disabled,
+	buttonText,
+}) => {
+	return (
+		<Wrapper>
+			<NavBar backIcon />
+			<SubTitle description={description}>{subTitleText}</SubTitle>
+			{children}
+			<ButtonArea>
+				<Link to={nextStepLink}>
+					<Button disabled={disabled}>{buttonText}</Button>
+				</Link>
+			</ButtonArea>
+		</Wrapper>
+	)
+}
+
+export default CreatingTimerLayout
+
+export const Wrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	width: 100%;
+	height: 100%;
+	align-items: center;
+	padding-bottom: 3.3rem;
+`
+
+export const Section = styled.section`
+	display: flex;
+	height: 50rem;
+	gap: 1rem;
+	justify-content: space-between;
+	align-items: center;
+	justify-content: center;
+`
+
+export const ButtonArea = styled.div`
+	display: flex;
+	justify-content: center;
+	position: absolute;
+	margin: 2rem 2rem;
+	width: 100%;
+	bottom: calc(env(safe-area-inset-bottom) + 1rem);
+`
+
+export const FormWrapper = styled.div`
+	margin-top: 22rem;
+`
